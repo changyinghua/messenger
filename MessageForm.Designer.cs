c729b3c3cc803a1657152b40581bdb660ac14b3c
Peter <yinghua.chang@acs.com.tw>
2017-05-19 14:14:22 +0800

    initialize

---
+++ MessageForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Messenger
+{
+    partial class MessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MessageForm));
+            this.messageRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.typeTextBox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageRichTextBox
+            // 
+            resources.ApplyResources(this.messageRichTextBox, "messageRichTextBox");
+            this.messageRichTextBox.Name = "messageRichTextBox";
+            this.messageRichTextBox.ReadOnly = true;
+            this.messageRichTextBox.TextChanged += new System.EventHandler(this.OnMessageRichTextBox_TextChanged);
+            // 
+            // typeTextBox
+            // 
+            resources.ApplyResources(this.typeTextBox, "typeTextBox");
+            this.typeTextBox.Name = "typeTextBox";
+            this.typeTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnTypeTextBox_KeyDown);
+            this.typeTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.OnTypeTextBox_KeyUp);
+            // 
+            // enterButton
+            // 
+            resources.ApplyResources(this.enterButton, "enterButton");
+            this.enterButton.Name = "enterButton";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.OnEnterButton_Click);
+            // 
+            // MessageForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.typeTextBox);
+            this.Controls.Add(this.messageRichTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MessageForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MessageForm_FormClosed);
+            this.Load += new System.EventHandler(this.MessageForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox messageRichTextBox;
+        private System.Windows.Forms.TextBox typeTextBox;
+        private System.Windows.Forms.Button enterButton;
+    }
+}
+
---
+++ MessageForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace Messenger
+{
+    public partial class MessageForm : Form
+    {
+        private string machineName = Environment.MachineName;
+
+        private MqttClient client;
+        public MessageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTypeTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !this.typeTextBox.Text.Equals(""))
+            {
+                //messageRichTextBox.AppendText(machineName+"："+typeTextBox.Text);
+                byte[] byt = Encoding.GetEncoding("Big5").GetBytes(machineName + "：" + typeTextBox.Text);
+                client.Publish("chat", byt);
+                typeTextBox.Clear();
+            }
+        }
+
+        private void OnTypeTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((int)e.KeyCode == 13)
+            {
+                e.SuppressKeyPress = true;
+            }
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                return;
+            }
+        }
+
+        private void OnMessageRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            messageRichTextBox.SelectionStart = messageRichTextBox.TextLength;
+
+            messageRichTextBox.ScrollToCaret();
+        }
+
+        private void OnEnterButton_Click(object sender, EventArgs e)
+        {
+            // messageRichTextBox.AppendText(machineName + "：" + typeTextBox.Text);
+            byte[] byt = Encoding.GetEncoding("Big5").GetBytes(machineName + "：" + typeTextBox.Text);
+            client.Publish("chat", byt);
+            typeTextBox.Clear();
+        }
+
+        private void MessageForm_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = typeTextBox;
+
+            try
+            {
+                client = new MqttClient(ConfigurationManager.AppSettings["broker"]);
+
+                string clientId = Guid.NewGuid().ToString();
+                client.Connect(clientId);
+
+                client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+                // subscribe to the topic "/home/temperature" with QoS 2
+                client.Subscribe(new string[] { "chat" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("error : " + ex.Message);
+                Environment.Exit(0);
+            }         
+        }
+
+        private void Client_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            string msg = System.Text.Encoding.GetEncoding("Big5").GetString(e.Message);
+            messageRichTextBox.Invoke(
+                new EventHandler(
+                    delegate
+                    {
+                        messageRichTextBox.AppendText(msg + "\n");
+                    }
+                    )
+                    );
+
+        }
+
+        private void MessageForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+            client.Disconnect();
+            Environment.Exit(0);
+        }
+
+    }
+}
